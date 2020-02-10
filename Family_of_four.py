645a2fb0dedbd7d5368bdc2e7d3b90a0121896a7
Oleh Olekshii <oooleks@gmail.com>
2020-02-10 15:35:58 -0800

    Update Family_of_four.py

--- Family_of_four.py
+++ Family_of_four.py
@@ -5,22 +5,13 @@ def put_family_in(n ,s):
     print("Reserved seats: ", seats)
     col = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K"]
     Paterns = ["BCDE", "DEFG", "FGHJ"]
-    k = []
-    
-    for i in range(1, n+1):
-      for x in col:
-        k.append(str(i)+str(x))
-        
-    for i in k:
-      if i in seats:
-        k.pop(k.index(i))
-       
     thisdict = {}
+
     for i in range(1, n+1):
       thisdict.update({i:''})
-      for y in k:
-        if y.strip("ABCDEFGHJK") == str(i):
-          thisdict[i] = thisdict[i] + y.strip("0123456789")
+      for x in col:
+        if str(i)+str(x) not in seats:
+          thisdict[i] = thisdict[i] + x
     print("Available row/seat: ", thisdict)
          
     for y in thisdict.values():
@@ -30,8 +21,8 @@ def put_family_in(n ,s):
       elif Paterns[0] in y or Paterns[1] in y or Paterns[2] in y:
         r += 1
     
-    print(r, "families of 4 can fit")  
+    print(r, "families of 4 can be seated")  
   else:
     print("Please, correct number of rows (n)!")
 
-put_family_in(6, "1A 2F 1C")
+put_family_in(2, "1A 2F 1C")
